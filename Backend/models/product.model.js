b7e301b36bf9523f5015eba48dd0ebdd09d89cfe
hanhan0211 <hanhanjk04@gmail.com>
2025-10-18 20:32:12 +0700

    worked on models

---
+++ Backend/models/product.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+const ProductSchema = mongoose.Schema({
+
+    title:{
+        type: String,
+        require: true,
+    },
+    desc:{
+        type: String,
+        require: true,
+    },
+    whatinbox:{
+        type: String
+    },
+    img:{
+        type: String,
+        require: true,
+    },
+    originalPrice:{
+        type: Number
+    },
+    wholesalePrice:{
+        type: Number
+    },
+    wholesaleMinimumQuantity:{
+        type: Number
+    },
+    categories:{
+        type: Array
+    },
+    discountedPrice:{
+        type: Number
+    },
+    inStock:{
+        type: Boolean,
+        default: true,
+    },
+    ratings:[ 
+        {
+            star: {type: String},
+            name: {type: String},
+            comment: {type: String},
+            postedBy: {type: String}
+        }
+    ]
+      
+    
+});
+ const Product = mongoose.model("Product", ProductSchema);
+ export default Product;
